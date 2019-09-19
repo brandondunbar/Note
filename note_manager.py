45c6a68b150426ea9e76d7fda541bc24d2a6784f
Brandon Dunbar <brandon.dunbar97@gmail.com>
2019-09-19 13:23:49 -0500

    sqlite3
    Implemented local sqlite3 database, fixed scrollboxes for main and notebook screens

--- note_manager.py
+++ note_manager.py
@@ -1,42 +1,163 @@
-"""A script that serializes and deserializes a dictionary.
-
-Usage:
-    import note_manager
-    notes = note_manager.load()
-    note_manager.save(notes)
+"""Interfaces with the sqlite database
 """
 
-import pickle
+import sqlite3
+import datetime
+
+
+def init_db():
+    """Creates database file and schema if necessary"""
+
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
+
+    with conn:
+
+        c.execute(
+            "SELECT COUNT(*) FROM sqlite_master "
+            "WHERE type='table' AND name='note_objs';")
+
+        exists = c.fetchall()[0][0]
+        current_time = datetime.datetime.now()
+
+        if not exists:
+
+            c.execute("""CREATE TABLE note_objs (
+               id INTEGER PRIMARY KEY,
+               name VARCHAR(100) NOT NULL,
+               last_modified VARCHAR(100),
+               data BLOB,
+               parent_id INT, 
+               FOREIGN KEY(parent_id) REFERENCES note_objs(id) 
+               ON DELETE CASCADE
+            );""")
+
+            c.execute(f"""INSERT INTO note_objs(
+            name, 
+            last_modified, 
+            data, 
+            parent_id) 
+            VALUES (
+            'My Notes', 
+            '{current_time}', 
+            'Notebook', 
+            NULL);""")
+
+
+def new_obj(name, data, parent_nb, modified=str(datetime.datetime.now())):
+    """Create a new note object inside the provided notebook"""
+
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
+
+    if "sqlite_" in name.lower():
+        raise ValueError("sqlite_ is reserved for internal use.")
+
+    with conn:
+        c.execute(f"""INSERT INTO note_objs(
+        name, 
+        last_modified, 
+        data, 
+        parent_id) 
+        VALUES (
+        '{name}', 
+        '{modified}', 
+        '{data}', 
+        '{parent_nb}');""")
+
+    conn.commit()
+    conn.close()
+
+
+def update_obj(obj_id, **kwargs):
+    """Updates a row with provided information"""
+
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
+
+    modified = str(datetime.datetime.now())
+
+    # Formats the kwargs to set values in query -------------------------------
+
+    sql_string = ''
+
+    for column, value in kwargs.items():
+        sql_string += f"{column} = '{value}', "
+
+    # Update query ------------------------------------------------------------
+    with conn:
+        c.execute("UPDATE note_objs "
+                  "SET " + sql_string +
+                  "last_modified = '" + modified + "' "
+                  "WHERE id = '" + str(obj_id) + "'")
+
+    conn.commit()
+    conn.close()
+
+
+def get_row(obj_id):
+    """Returns the row with the given id."""
+
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
+
+    with conn:
+        c.execute(f"SELECT * FROM note_objs WHERE id = {obj_id}")
+        return c.fetchone()
+
+
+def get_children(obj_id):
+    """Gets the children rows of the provided row, via row ID"""
+
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
+
+    with conn:
+        c.execute(f"SELECT * FROM note_objs WHERE parent_id = {obj_id}")
+        children = c.fetchall()
+        return children
 
 
 def load():
-    """Loads the data object stored in serial_notes.pickle in the local
-    directory.
+    """Returns the entire table as list of tuples(rows)"""
 
-    Returns:
-        Either the loaded object (typically a dictionary of notes in the format
-        {'Notebook Name': [<Note Object>, <Note Object>,], '...':[...],}) or
-        an empty dictionary to be used for the same purpose.
-    """
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
 
-    try:
-        with open('serial_notes.pickle', "rb") as file_object:
-            notes = pickle.load(file_object)
-            return notes
+    with conn:
+        c.execute("SELECT * FROM note_objs")
 
-    except FileNotFoundError:
-        return {}
+    return c.fetchall()
 
 
-def save(notes):
-    """Serializes an object into the serial_notes.pickle file.
+def delete(obj_id):
+    """Delete note object"""
 
-    Args:
-        notes: A dictionary object in the format:
-        {'Notebook Name': [<Note Object>, <Note Object>,], '...':[...],}
-    """
+    # Connects to database file, if db file doesn't exist it creates one
+    conn = sqlite3.connect("notes.db")
+    # Cursor to execute sql commands
+    c = conn.cursor()
 
-    # Serialize
-    file_name = "serial_notes.pickle"
-    with open(file_name, "wb") as file_object:
-        pickle.dump(notes, file_object)
+    with conn:
+
+        c.execute(f"DELETE FROM note_objs "
+                  f"WHERE id = {obj_id}")
+
+        # Cascade delete doesn't seem to be triggering, manually doing it:
+        c.execute(f"DELETE FROM note_objs "
+                  f"WHERE parent_id = {obj_id}")
+
+
+init_db()  # Initialize database
